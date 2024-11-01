b884146d491c1aa605d5d77046dfec72ddc586bc
Hlieb Kotin <gleb.kotiun@gmaill.com>
2024-11-01 17:41:32 +0100

    added burger menu script

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-// * * * Backdrop script
+// * * * Backdrop script * * *
 const backdrop = document.querySelector(".backdrop");
 const orderBtn = document.querySelector(".order-button");
 const closeBackdrop = document.querySelector(".close-btn");
@@ -14,3 +14,20 @@ const backdropClose = () => {
 orderBtn.addEventListener("click", backdropOpen);
 
 closeBackdrop.addEventListener("click", backdropClose);
+
+// * * * Mobile menu * * *
+
+const mobileMenu = document.querySelector(".mobile-menu");
+const openMobileMenu = document.querySelector(".close-btn-menu");
+const closeMobileMenu = document.querySelector(".burger-close-btn");
+
+const mobileMenuOpen = () => {
+  mobileMenu.classList.add("is-open");
+};
+
+const mobileMenuClose = () => {
+  mobileMenu.classList.remove("is-open");
+};
+
+openMobileMenu.addEventListener("click", mobileMenuOpen);
+closeMobileMenu.addEventListener("click", mobileMenuClose);
