615855918e3089d35c80b7e70988fd4dd1a7e065
Hlieb Kotin <gleb.kotiun@gmaill.com>
2024-11-06 22:50:13 +0100

    added js for burger menu

--- file.js
+++ file.js
@@ -1,37 +1,41 @@
 // * * * Backdrop script * * *
+
 const backdrop = document.querySelector(".backdrop");
 const orderBtn = document.querySelector(".order-button");
-const closeBackdrop = document.querySelector(".close-btn");
+const closeBackdropBtn = document.querySelector(".close-btn");
 
-const backdropOpen = () => {
+orderBtn.addEventListener("click", openBackdrop);
+closeBackdropBtn.addEventListener("click", closeBackdrop);
+
+function openBackdrop(event) {
   backdrop.classList.add("is-open");
-};
+}
 
-const backdropClose = () => {
+function closeBackdrop(event) {
   backdrop.classList.remove("is-open");
-};
+}
 
-orderBtn.addEventListener("click", backdropOpen);
-
-closeBackdrop.addEventListener("click", backdropClose);
-
-// * * * Mobile menu * * *
+// // * * * Mobile menu * * *
 
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 const openMobileMenu = document.querySelector(".close-btn-menu");
 const closeMobileMenu = document.querySelector(".burger-close-btn");
 
-const mobileMenuOpen = () => {
+openMobileMenu.addEventListener("click", mobileMenuOpener);
+mobileMenuContainer.addEventListener("click", mobileMenuCloser);
+
+function mobileMenuOpener() {
   mobileMenu.classList.add("is-open");
-};
+}
 
-const mobileMenuClose = () => {
+function mobileMenuCloser(event) {
+  if (
+    !event.target.classList.contains("mobile-nav-list-link") &&
+    !event.target.closest(".burger-close-btn")
+  ) {
+    return;
+  }
+
   mobileMenu.classList.remove("is-open");
-};
-
-openMobileMenu.addEventListener("click", mobileMenuOpen);
-closeMobileMenu.addEventListener("click", mobileMenuClose);
-
-// const mobileNavLinks = document.querySelectorAll(".mobile-nav-list-link");
-
-// mobileNavLinks.addEventListener("click", mobileMenuClose);
+}
